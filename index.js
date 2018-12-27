85206fe2096d3922ba2205417a5390cf1fbe2241
Jonathan Liu <liu.z.jonathan@gmail.com>
2018-12-26 16:12:16 -0800

    node js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    var url = 'https://accounts.google.com/o/oauth2/auth?' + 
+					'client_id=946689392269-rd0qkinhi24uv8q7kf7pc981sd0vm9mf.apps.googleusercontent.com&' +
+					'redirect_uri=http%3A%2F%2Flocalhost:1266%2Foauth2callback&' + 
+					'scope=https://www.googleapis.com/auth/youtube&response_type=token';
+    res.redirect(url);
+    //res.sendFile(__dirname + '/www/index.html');
+});
+
+app.get('/oauth2callback', (req, res) => {
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+http.listen(process.env.PORT || 1266, function() {
+    var port = http.address().port;
+    console.log('Server listening on port ', port)
+});
