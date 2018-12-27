1a7fe2c47009f9f3fa2b81e2f0b9ffbfc348bc2f
Arjun Patrawala <ajpat1234@gmail.com>
2018-12-26 16:29:01 -0800

    a

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
+app.use(express.static(__dirname + '/www/'));
+
 app.get('/', (req, res) => {
     var url = 'https://accounts.google.com/o/oauth2/auth?' + 
 					'client_id=946689392269-rd0qkinhi24uv8q7kf7pc981sd0vm9mf.apps.googleusercontent.com&' +
@@ -15,7 +17,7 @@ app.get('/oauth2callback', (req, res) => {
     res.sendFile(__dirname + '/www/index.html');
 });
 
-http.listen(process.env.PORT || 1266, function() {
+http.listen(process.env.PORT || 8080, function() {
     var port = http.address().port;
     console.log('Server listening on port ', port)
 });
