416160d97f5da39d36b61a48137f94cd8dd545a8
Jonathan Liu <liu.z.jonathan@gmail.com>
2018-12-28 11:57:23 -0800

    work!

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const url = require('url');
 
 app.use(express.static(__dirname + '/www/'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     var url = 'https://accounts.google.com/o/oauth2/auth?' + 
 					'client_id=946689392269-rd0qkinhi24uv8q7kf7pc981sd0vm9mf.apps.googleusercontent.com&' +
 					'redirect_uri=http%3A%2F%2Flocalhost:1266%2Foauth2callback&' + 
@@ -15,9 +16,13 @@ app.get('/', (req, res) => {
 
 app.get('/oauth2callback', (req, res) => {
     res.sendFile(__dirname + '/www/index.html');
+    console.log(req);
+    console.log(req.originalUrl);
+    console.log(url.parse(req.originalUrl));
 });
+*/
 
-http.listen(process.env.PORT || 8080, function() {
+http.listen(process.env.PORT || 1266, function() {
     var port = http.address().port;
     console.log('Server listening on port ', port)
 });
