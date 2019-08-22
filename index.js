b7d21bbd6589ed31fc8fa8e50ae8ea716c4020d5
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-08-21 22:14:18 -0700

    made changing webcam easier and should work now?
    -removing -rtbufsize might help???

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/start_streaming', (req, res) => {
         if (err) {
             console.log(err);
 
-            var firstQuote = stderr.indexOf('"');
+            /*var firstQuote = stderr.indexOf('"');
             var secondQuote = stderr.indexOf('"', firstQuote+1);
             var inputVidName = stderr.substring(firstQuote+1, secondQuote);
             console.log('input vid: ', inputVidName);
@@ -21,10 +21,24 @@ app.post('/start_streaming', (req, res) => {
             var thirdQuote = stderr.indexOf('"', audio);
             var fourthQuote = stderr.indexOf('"', thirdQuote+1);
             var micName = stderr.substring(thirdQuote+1, fourthQuote);
-            console.log('mic: ', micName);
+            console.log('mic: ', micName);*/
+
+            const webcam1 = {
+                name: 'USB_Camera',
+                resolution: '1920x1080',
+                framerate: '30',
+            };
+            const webcam2 = {
+                name: 'USB Camera',
+                resolution: '1920x1080',
+                framerate: '30',
+            };
+            const micName = 'Microphone (C-Media USB Audio Device   )';
             
-            const horizontalStackCmd = 'ffmpeg -y -f dshow -video_size 1920x1080 -rtbufsize 702000k -framerate 30 -i video="USB_Camera":audio="Microphone Array (Realtek High Definition Audio(SST))" -f dshow -video_size 1280x720 -rtbufsize 702000k -framerate 30 -i video="USB Video Device" -i ./img/ignition.png -filter_complex "[0:v]pad=iw+1280:ih[int];[int][1:v]overlay=W-w:0[int2];[int2][2:v]overlay=W-w:H-h[vid]" -map [vid] -map 0:a -copyts -c:v libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -g 60 -c:a aac -b:a 128k -ar 44100 -f flv "'+ req.body.rtmpAddr +'"';
-            const middleSplitCmd = 'ffmpeg -y -f dshow -video_size 1920x1080 -rtbufsize 702000k -framerate 30 -i video="USB_Camera":audio="Microphone Array (Realtek High Definition Audio(SST))" -f dshow -video_size 1280x720 -rtbufsize 702000k -framerate 30 -i video="USB Video Device" -i ./img/ignition_small.png -filter_complex "[0:v]crop=iw/3:ih:0:0[v0_left];[0:v]crop=iw/3:ih:2*iw/3:0[v0_right];[v0_left]pad=5*iw:ih[int];[1:v]scale=1920x1080[v1];[int][v1]overlay=w/3:0[int2];[int2][v0_right]overlay=4*W/5:0[int3];[int3][2:v]overlay=W-w:H-h[vid]" -map [vid] -map 0:a -copyts -c:v libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -g 60 -c:a aac -b:a 128k -ar 44100 -f flv "'+ req.body.rtmpAddr +'"';
+            // horizontalStackCmd stacks the view from webcam1 to the left of the view from webcam2
+            const horizontalStackCmd = 'ffmpeg -y -f dshow -video_size '+ webcam1.resolution +' -framerate '+ webcam1.framerate +' -i video="'+ webcam1.name +'":audio="'+ micName +'" -f dshow -video_size '+ webcam2.resolution +' -framerate '+ webcam2.framerate +' -i video="'+ webcam2.name +'" -i ./img/ignition.png -filter_complex "[0:v]pad=iw+1280:ih[int];[int][1:v]overlay=W-w:0[int2];[int2][2:v]overlay=W-w:H-h[vid]" -map [vid] -map 0:a -copyts -c:v libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -g 60 -c:a aac -b:a 128k -ar 44100 -f flv "'+ req.body.rtmpAddr +'"';
+            // middleSplitCmd splits the view from webcam1 and puts it to the sides of the video, and places the view from webcam2 in the middle
+            const middleSplitCmd = 'ffmpeg -y -f dshow -video_size '+ webcam1.resolution +' -framerate '+ webcam1.framerate +' -i video="'+ webcam1.name +'":audio="'+ micName +'" -f dshow -video_size '+ webcam2.resolution +' -framerate '+ webcam2.framerate +' -i video="'+ webcam2.name +'" -i ./img/ignition_small.png -filter_complex "[0:v]crop=iw/3:ih:0:0[v0_left];[0:v]crop=iw/3:ih:2*iw/3:0[v0_right];[v0_left]pad=5*iw:ih[int];[1:v]scale=1920x1080[v1];[int][v1]overlay=w/3:0[int2];[int2][v0_right]overlay=4*W/5:0[int3];[int3][2:v]overlay=W-w:H-h[vid]" -map [vid] -map 0:a -copyts -c:v libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -g 60 -c:a aac -b:a 128k -ar 44100 -f flv "'+ req.body.rtmpAddr +'"';
             
             exec(middleSplitCmd, 
                 (err, stdout, stderr) => {
